80068228ba0091c347e03888ea79109705320706
anatoleoN1 <anatole.oerlemans@gmail.com>
2025-09-01 16:03:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ import inquirer from "inquirer";
 import fetch from "node-fetch";
 
 // ----------------- UPDATE FUNCTION ----------------
-const LOCAL_VERSION = "1.2.34";
+const LOCAL_VERSION = "1.2.35";
 
 async function checkForUpdates() {
   try {
